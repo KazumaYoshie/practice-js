ccb05788e8c1722c19666b233d93f520d7be082c
KazumaYoshie <yosh1kazu217629@gmail.com>
2021-12-23 00:23:32 +0900

    [Add]if

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
-let str1 = 'Hello';
-let str2 = 'World';
-alert(str1 + str2);
+let orange = '100';
+let apple = '120';
+
+if (orange < apple) {
+  alert('みかんはりんごより安い');
+} else if (orange == apple) {
+  alert('みかんとりんごは同じ値段');
+} else{
+  alert('みかんはりんごより高い')
+}
 
 
